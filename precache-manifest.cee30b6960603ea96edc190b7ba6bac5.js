0326e54c0db23a557652facaaf35ab285bde2027
melissa <melissazhu223@gmail.com>
2018-10-24 00:19:51 +1100

    Updates

---
+++ precache-manifest.cee30b6960603ea96edc190b7ba6bac5.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/GamePlayNine/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b0cff39accfd9076b891",
+    "url": "/GamePlayNine/static/css/main.8cf95cca.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/GamePlayNine/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a0917b014c1c1144fcc8",
+    "url": "/GamePlayNine/static/js/1.a0917b01.chunk.js"
+  },
+  {
+    "revision": "d573a6228998d2e30b2e",
+    "url": "/GamePlayNine/static/js/runtime~main.d573a622.js"
+  },
+  {
+    "revision": "b0cff39accfd9076b891",
+    "url": "/GamePlayNine/static/js/main.b0cff39a.chunk.js"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/GamePlayNine/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/GamePlayNine/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/GamePlayNine/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "a0917b014c1c1144fcc8",
+    "url": "/GamePlayNine/static/css/1.245fc768.chunk.css"
+  },
+  {
+    "revision": "e00a28eec1b75b8a82c762e4482ed458",
+    "url": "/GamePlayNine/index.html"
+  }
+];
